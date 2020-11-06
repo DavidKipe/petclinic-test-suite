b96da5718dbd83c45e1dd51f7836dc773515ff52
Davide <david91kipe@gmail.com>
2020-11-06 16:27:16 +0100

    Added one more test about duplicate Pet name

--- src/test/java/assertions/AddPetTests.java
+++ src/test/java/assertions/AddPetTests.java
@@ -13,19 +13,19 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class AddPetTests extends BaseTests {
 
 	private final String lastName = "Black";
+	private final String petBirthDate = "2017-11-30";
 
 	@Test
 	void testAddPetToOwner() {
-		String name = "Arnold";
-		String birthDate = "2017-11-30";
+		String petName = "Arnold";
 
 		AddEditPetPO addEditPetPO = homePO.goToFindOwner().searchFor(lastName).addPet();
 
-		OwnerPO ownerPO = addEditPetPO.fillFieldsAndSubmit(name, birthDate);
+		OwnerPO ownerPO = addEditPetPO.fillFieldsAndSubmit(petName, petBirthDate);
 
 		List<Pet> pets = ownerPO.getPets();
 
-		assertTrue(pets.stream().anyMatch(p -> p.name.equals(name) && p.birthDate.equals(birthDate)));
+		assertTrue(pets.stream().anyMatch(p -> p.name.equals(petName) && p.birthDate.equals(petBirthDate)));
 		// NOTE fail if it is not a clean run because the name is already in use (these tests don't clean up)
 		// NOTE fail on OLD version because the submit button is underneath the date picker
 	}
@@ -38,6 +38,17 @@ class AddPetTests extends BaseTests {
 
 		assertEquals("is required", addEditPetPO.getNameError());
 		assertEquals("is required", addEditPetPO.getBirthDateError());
+	}
+
+	@Test
+	void testAddPetToOwnerDuplicateName() {
+		String petName = "Lucky";
+
+		AddEditPetPO addEditPetPO = homePO.goToFindOwner().searchFor(lastName).addPet();
+
+		addEditPetPO.fillFieldsAndSubmit(petName, petBirthDate);
+
+		assertEquals("is already in use", addEditPetPO.getNameError());
 	}
 
 }
--- src/test/java/recheck/explicit/AddPetTests.java
+++ src/test/java/recheck/explicit/AddPetTests.java
@@ -6,16 +6,16 @@ import pageobject.AddEditPetPO;
 class AddPetTests extends BaseTests {
 
 	private final String lastName = "Black";
+	private final String petBirthDate = "2017-11-30";
 
 	@Test
 	void testAddPetToOwner() {
 		startTest("addPetToOwner");
 
-		String name = "Arnold";
-		String birthDate = "2017-11-30";
+		String petName = "Arnold";
 
 		AddEditPetPO addEditPetPO = homePO.goToFindOwner().searchFor(lastName).addPet();
-		addEditPetPO.fillFieldsAndSubmit(name, birthDate);
+		addEditPetPO.fillFieldsAndSubmit(petName, petBirthDate);
 
 		// NOTE fail if it is not a clean run because the name is already in use (these tests don't clean up)
 		// NOTE fail on OLD version because the submit button is underneath the date picker
@@ -31,6 +31,19 @@ class AddPetTests extends BaseTests {
 		addEditPetPO.submit();
 
 		endTest("addPetToOwnerWithEmptyFields");
+	}
+
+	@Test
+	void testAddPetToOwnerDuplicateName() {
+		startTest("addPetToOwnerDuplicateName");
+
+		String petName = "Lucky";
+
+		AddEditPetPO addEditPetPO = homePO.goToFindOwner().searchFor(lastName).addPet();
+
+		addEditPetPO.fillFieldsAndSubmit(petName, petBirthDate);
+
+		endTest("addPetToOwnerDuplicateName");
 	}
 
 }
